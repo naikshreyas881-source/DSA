f59667214097b5600bbd3ed6ecc74c6b49d48dbc
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-01 18:47:52 +0530

    Update Basic Maths Logic Buildup.py

--- Basic Maths Logic Buildup.py
+++ Basic Maths Logic Buildup.py
@@ -5,9 +5,15 @@ while num>0:
     print(last_digit)
     num=num//10
 print(num%10)
-# output=
-0
-7
-8
-5
-0
+# output= 5 8 7 0
+
+
+#Count the number of Digits in an Integer
+ num=75443467
+ count=0
+
+ while num>0:
+     count+=1
+     num=num//10
+    print(count)
+# output=8
