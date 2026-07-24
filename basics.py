280f70b9b670cafe04f327eb65046006cd689ea8
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-24 16:56:13 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -22,3 +22,17 @@ for num in m:
         if x==num:
             count+=1
     print(count)
+
+#Print the count of each element in the same order as it appears in m
+# n = [5,3,2,2,1,5,5,7,5,10]
+# m = [10,111,1,9,5,67,2]  using hash method 
+n=[5,3,2,2,1,5,5,7,5,10]
+m=[10,111,1,9,5,67,2]
+hash_list=[0]*11
+for num in n:
+    hash_list[num]+=1
+for num in m:
+    if num<1 or num>10:
+       print(0)
+    else:
+       print(hash_list[num])
