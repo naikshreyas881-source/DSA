2846279f2c2921ff31989addba5f80df706398c1
Shreyas Naik <naikshreyas881@gmail.com>
2026-08-12 08:07:48 +0530

    Implement selection sort for decreasing order
    Added a selection sort implementation to sort an array in decreasing order with example input and output.

--- sorting of the algorithm.py
+++ sorting of the algorithm.py
@@ -18,3 +18,20 @@ class Solution:
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
         return arr
+
+# Given an unsorted array of size n, use selection sort to sort the array arr[] in decreasing order.
+# Input: arr = [1,3,5,2,4,6,8,7,9]
+# Output: [9, 8, 7, 6, 5, 4, 3, 2, 1]
+# Explanation: After sorting the array in increasing order using selection sort, we get 9, 8, 7, 6, 5, 4, 3, 2, 1
+
+arr=[1,3,5,2,4,6,8,7,9]
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(0, n):
+        min_idx = i;
+        for j in range(i + 1, n):
+            if arr[j] > arr[min_idx]:
+                min_idx = j;
+        arr[i],arr[min_idx] = arr[min_idx],arr[i]
+    return arr
+print(selection_sort(arr))
