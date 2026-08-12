fd9d97ac616b68e13bcb5239e2c291f5ca10ef7e
Shreyas Naik <naikshreyas881@gmail.com>
2026-08-12 14:11:18 +0530

    Update sorting of the algorithm.py

--- sorting of the algorithm.py
+++ sorting of the algorithm.py
@@ -51,3 +51,25 @@ def selection_sort(arr):
     return arr
 print(selection_sort(arr))
 
+#count the swaps
+arr = [4, 3, 2, 1]
+
+def selection_sort(arr):
+    n = len(arr)
+    count = 0
+
+    for i in range(n):
+        min_idx = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            count += 1
+
+    return arr, count
+
+print(selection_sort(arr))
+
