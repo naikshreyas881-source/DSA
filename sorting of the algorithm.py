e26ae8b7b183c8bd71abeff833684c100d00ccd2
Shreyas Naik <naikshreyas881@gmail.com>
2026-08-14 19:11:13 +0530

    Update sorting of the algorithm.py

--- sorting of the algorithm.py
+++ sorting of the algorithm.py
@@ -68,6 +68,20 @@ def selection_sort(arr):
         if min_idx != i:
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
             count += 1
+# Given an array arr, sort the array using bubble sort algorithm.
+# Input: arr = [4, 1, 3, 9, 7]
+# Output: [1, 3, 4, 7, 9]
+    from typing import List
+
+class Solution:
+    def bubbleSort(self, arr: List[int]) -> List[int]:
+        pass
+        n=len(arr)
+        for i in range(n-2,-1,-1):
+            for j in range(0,i+1):
+                if arr[j]>arr[j+1]:
+                    arr[j],arr[j+1]=arr[j+1],arr[j]
+    
 
     return arr, count
 
