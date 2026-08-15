5d0642d4c4e8ff744d17d2ee6c8c8deb63f84634
Shreyas Naik <naikshreyas881@gmail.com>
2026-08-15 07:18:11 +0530

    Update sorting of the algorithm.py

--- sorting of the algorithm.py
+++ sorting of the algorithm.py
@@ -83,4 +83,14 @@ class Solution:
                 if arr[j]>arr[j+1]:
                     arr[j],arr[j+1]=arr[j+1],arr[j]
 
-
+# slove by bubble sort
+#input arr=[5, 2, 8, 1, 3]
+arr=[5, 2, 8, 1, 3]
+def bubblesort(arr):
+    n=len(arr)
+    for i in range(n-2,-1,-1):
+        for j in range(0,i+1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+print(bubblesort(arr))
