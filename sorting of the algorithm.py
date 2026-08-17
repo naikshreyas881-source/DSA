127a47a978da2129d17c9f14e9a40468a8c74ba9
Shreyas Naik <naikshreyas881@gmail.com>
2026-08-17 08:48:17 +0530

    Update sorting of the algorithm.py

--- sorting of the algorithm.py
+++ sorting of the algorithm.py
@@ -136,3 +136,16 @@ def bubblesort(arr):
 
     return arr,count
 print(bubblesort(arr))
+
+# Input: arr[] = [4, 1, 3, 9, 7] sort the array using insertion sort 
+class Solution:
+    def insertionSort(self, arr):
+        n=len(arr)
+        for i in range (1,n):
+            key=arr[i]
+            j=i-1
+            while j>=0 and arr[j]>key:
+                arr[j+1]=arr[j]
+                j-=1
+            arr[j+1]=key    
+        
