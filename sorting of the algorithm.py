473d51d9ab4fd2492d982a3bfb1054772298b31a
Shreyas Naik <naikshreyas881@gmail.com>
2026-08-17 09:22:09 +0530

    Update sorting of the algorithm.py

--- sorting of the algorithm.py
+++ sorting of the algorithm.py
@@ -147,5 +147,22 @@ class Solution:
             while j>=0 and arr[j]>key:
                 arr[j+1]=arr[j]
                 j-=1
-            arr[j+1]=key    
+            arr[j+1]=key 
+
+# arr=[5, 2, 8, 1, 3] sort the arry using insertion sort
+arr=[5, 2, 8, 1, 3]
+class Solution:
+    def insertionSort(self, arr):
+        n = len(arr)
+        for i in range(1, n):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr
+solution = Solution()
+print(solution.insertionSort(arr))
+
         
